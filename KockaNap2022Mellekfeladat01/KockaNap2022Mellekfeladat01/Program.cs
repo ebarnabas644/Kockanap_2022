65b571e454ddbe7c3c3c5670a0f619ec541aaa14
Básthy Ádám László <basthyadi@gmail.com>
2022-11-04 18:18:59 +0100

    A possible solution

--- KockaNap2022Mellekfeladat01/KockaNap2022Mellekfeladat01/Program.cs
+++ KockaNap2022Mellekfeladat01/KockaNap2022Mellekfeladat01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -28,15 +29,15 @@ namespace KockaNap2022Mellekfeladat01
 
     internal class StarSystem
     {
-        private string _name;
+        private string name;
         private List<int> _starPowers;
 
-        private int _maxStarPower = 0;
+        private int maxStarPower = 0;
 
         public StarSystem(string name, List<int> starPowers)
         {
-            _name = name;
-            _starPowers = starPowers;
+            this.name = name;
+            this._starPowers = starPowers;
         }
 
         public static List<StarSystem> GetInput(string[] input)
@@ -57,14 +58,22 @@ namespace KockaNap2022Mellekfeladat01
                     listStarsystem.Add(new StarSystem(input[i - 2].Substring(1, input[i - 2].Length - 2), stars));
                 }
             }
+            List<int> stas = new List<int>();
+            string[] powes = input[input.Length - 1].Split(';');
+            foreach (var power in powes)
+            {
+                stas.Add(int.Parse(power));
+            }
+            listStarsystem.Add(new StarSystem(input[input.Length - 2].Substring(1, input[input.Length - 2].Length - 2), stas));
+
 
             return listStarsystem;
         }
         public static void CalculateAll(List<StarSystem> starSystems)
         {
-            foreach (var systems in starSystems)
+            foreach (var system in starSystems)
             {
-                systems.CalculatePower();
+                StarSystem.CalculatePower(system);
             }
         }
 
@@ -79,15 +88,36 @@ namespace KockaNap2022Mellekfeladat01
             return systems.ToArray();
         }
 
-        public void CalculatePower()
+        public static void CalculatePower(StarSystem system)
         {
-
+            while (system._starPowers.Count > 2)
+            {
+                List<int> _maxs = new List<int>();
+                for (int i = 1; i < system._starPowers.Count - 1; i++)
+                {
+                    _maxs.Add(system._starPowers[i - 1] * system._starPowers[i + 1]);
+                }
+                //int maxValue = _maxs.Max();
+                int idx = _maxs.IndexOf(_maxs.Max());
+                if (_maxs.Count > 1)
+                {
+                    for (int i = 0; i < _maxs.Count; i++)
+                    {
+                        if (_maxs[i] == _maxs[idx] && system._starPowers[i + 1] < system._starPowers[idx + 1])
+                        {
+                            idx = i;
+                        }
+                    }
+                }
+                system.maxStarPower += _maxs[idx];
+                system._starPowers.RemoveAt(idx);
+            }
         }
 
 
         public override string ToString()
         {
-            return string.Format("{0}: {1}", _name, _maxStarPower);
+            return string.Format("{0}: {1}", name, maxStarPower);
         }
     }
 }
