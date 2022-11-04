fc2dc8a47b5f51269be5a2238fbfc0547291eb47
Básthy Ádám László <basthyadi@gmail.com>
2022-11-04 21:08:35 +0100

    Finished solution by Sanyja2000

--- KockaNap2022Mellekfeladat03/KockaNap2022Mellekfeladat03/Program.cs
+++ KockaNap2022Mellekfeladat03/KockaNap2022Mellekfeladat03/Program.cs
@@ -12,8 +12,24 @@ namespace KockaNap2022Mellekfeladat03
         {
             int m = int.Parse(Console.ReadLine());
             int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Round(Calculate(m,n,100000),2).ToString("0.0"));
+        }
 
-
+        private static double Calculate(int M, int N, int it)
+        {
+            int s = 0;
+            int n = 0;
+            int m = 0;
+            for (int i = 0; i < it; i++)
+            {
+                m += M;
+                if (n < m)
+                {
+                    n += N;
+                }
+                s += n - m;
+            }
+            return (double)s / it;
         }
     }
 }
