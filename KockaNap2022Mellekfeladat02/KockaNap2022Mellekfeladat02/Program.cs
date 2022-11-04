1bcd1b2273b572f9622b8d3a85f2770bd7c180c4
Básthy Ádám László <basthyadi@gmail.com>
2022-11-04 19:03:03 +0100

    Add Base project

---
+++ KockaNap2022Mellekfeladat02/KockaNap2022Mellekfeladat02/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KockaNap2022Mellekfeladat02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> area = GetInput();
+
+            Console.WriteLine(Calculate(area));
+            Console.ReadKey();
+        }
+
+        static int Calculate(List<string> list)
+        {
+            if (list.Count == 1)
+            {
+                return 0;
+            }
+
+            int res = 0;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                for (int j = 0; j < list[i].Length; j++)
+                {
+                    int x = i;
+
+                    while(x >= 0 && list[x][j] != 'O')
+                    {
+                        x--;
+                    }
+                    if (x >= 0 && x != i)
+                    {
+                        res++;
+                    }
+                }
+            }
+            return res;
+
+        }
+        static List<string> GetInput()
+        {
+            List<string> map = new List<string>();
+            string sor = Console.ReadLine();
+            do
+            {
+                map.Add(sor);
+                sor = Console.ReadLine();
+                
+            } while (sor != "");
+
+            return map;
+        }
+    }
+}
