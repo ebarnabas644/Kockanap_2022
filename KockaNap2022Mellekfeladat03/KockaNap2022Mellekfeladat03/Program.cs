8efe3ac0d31bc0ec598241fe19f3c1654105f9c5
Básthy Ádám László <basthyadi@gmail.com>
2022-11-04 20:10:51 +0100

    Add Base project

---
+++ KockaNap2022Mellekfeladat03/KockaNap2022Mellekfeladat03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KockaNap2022Mellekfeladat03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
